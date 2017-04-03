1bd3b4832be44d4ef3ef15816e6ef792370d907a
caanerud <caanerud.ca@gmail.com>
2017-04-03 13:54:46 -0400

    added AnimalRepository class

---
+++ src/AnimalRepository.java
@@ -0,0 +1,31 @@
+/**
+ * Created by chrisaanerud on 4/3/17.
+ */
+public class AnimalRepository {
+
+    public void saveAnimal(Animal animal){
+
+
+    }
+
+    public void readByID(Animal animal){
+
+
+    }
+
+    public void saveAnimalEdits(Animal animal){
+
+
+    }
+
+    public void saveNewAnimals(Animal animal){
+
+
+    }
+
+    public void deleteAnimal(Animal animal){
+
+
+    }
+
+}
